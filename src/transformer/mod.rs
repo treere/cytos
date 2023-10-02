fdccc359c87ac018e08346bb0e75f55b54826754
treere <tomasiandrea.at@gmail.com>
2023-10-02 23:06:44 +0200

    Remove list dir

--- src/transformer/mod.rs
+++ src/transformer/mod.rs
@@ -1,10 +1,8 @@
 mod add_one;
 mod incremental;
-mod list_dir;
 
 pub use add_one::{AddValue, AddValueConfigInput, AddValueConfigOutput};
 pub use incremental::{IncrementalGenerator, IncrementalGeneratorConfigOutput};
-pub use list_dir::{ListDir, ListDirConfigOutput};
 
 use crate::architecture::Processor;
 
