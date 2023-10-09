2c15ed9afab8e6f5e1fe3a586c1d06453a73a982
treere <tomasiandrea.at@gmail.com>
2023-10-09 20:45:17 +0200

    Removed transformers

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
 pub mod architecture;
 pub mod loader;
-pub mod transformer;
 pub mod utils;
--- src/loader.rs
+++ src/loader.rs
@@ -1,6 +1,8 @@
-use serde::Deserialize;
+use crate::architecture::Graph as G;
+use crate::architecture::{Processor, Transformer};
 
-use crate::{architecture::Graph as G, transformer::Loader};
+use serde::Deserialize;
+use std::collections::HashMap;
 
 #[derive(Deserialize, Debug)]
 pub struct Graph {
@@ -37,4 +39,37 @@ pub struct Node {
 pub struct Link {
     pub src: (String, String),
     pub dst: (String, String),
+}
+
+pub struct Loader {
+    transformers: HashMap<String, Box<dyn Fn() -> Box<dyn Transformer>>>,
+}
+
+impl Loader {
+    pub fn new() -> Self {
+        Self {
+            transformers: HashMap::new(),
+        }
+    }
+    pub fn add<K: Transformer + 'static>(
+        mut self,
+        name: impl AsRef<str>,
+        f: impl (Fn() -> K) + 'static,
+    ) -> Self {
+        self.transformers
+            .entry(name.as_ref().to_owned())
+            .or_insert(Box::new(move || Box::new(f())));
+        self
+    }
+
+    pub fn load(&self, name: &str, typ: &str) -> Result<Processor, &'static str> {
+        let factory = self.transformers.get(typ).ok_or("missing type")?;
+        Ok(Processor::load(name.to_owned(), factory()))
+    }
+}
+
+impl Default for Loader {
+    fn default() -> Self {
+        Self::new()
+    }
 }
