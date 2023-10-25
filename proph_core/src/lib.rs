1623a1f2ac1b68c116b8000ff96b6454827e1697
treere <tomasiandrea.at@gmail.com>
2023-10-25 22:27:46 +0200

    Huffman tree

--- proph_core/src/lib.rs
+++ proph_core/src/lib.rs
@@ -76,23 +76,134 @@ fn marker_name(marker: u16) -> &'static str {
 }
 
 #[derive(Debug, PartialEq)]
-enum Node {
+pub enum Node {
     Value(u32),
     Split(u32, u32),
     None,
 }
 
-fn a(count: &[u8; 16], symbol: &[u8; 16]) {
-    let mut root = vec![Node::Split(1, 2), Node::None, Node::None];
-    let left_most = 0;
-    for c in count.iter() {
-        if *c == 0 {
-            let current = left_most;
-            while root[current] != Node::None {
-                let x = root.len();
-                root.push(Node::None);
-                root.push(Node::None);
+struct Point {
+    level: usize,
+    value: Node,
+}
+
+impl std::fmt::Debug for Point {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.value {
+            Node::Value(x) => write!(f, "<{}, {}>", self.level, x),
+            Node::Split(x, y) => write!(f, "<{}, {} {}>", self.level, x, y),
+            Node::None => write!(f, "<{}, -->", self.level),
+        }
+    }
+}
+
+pub struct HuffmanTree {
+    tree: Vec<Node>,
+}
+
+impl HuffmanTree {
+    pub fn new(count: &[u8], symbol: &[u32]) -> Self {
+        let mut last_value = 0;
+        let mut root = vec![
+            Point {
+                level: 0,
+                value: Node::Split(1, 2),
+            },
+            Point {
+                level: 1,
+                value: Node::None,
+            },
+            Point {
+                level: 1,
+                value: Node::None,
+            },
+        ];
+        let mut left_most = 1;
+        let mut symbol_offset = 0;
+
+        for (l, c) in count.iter().enumerate() {
+            let level = l + 1;
+
+            if *c == 0 {
+                let mut current = left_most;
+                let end = root.len();
+                while current < end {
+                    if root[current].level != level {
+                        current += 1;
+                        continue;
+                    }
+                    let pos = root.len();
+                    root[current].value = Node::Split(pos as u32, (pos + 1) as u32);
+                    root.push(Point {
+                        level: level + 1,
+                        value: Node::None,
+                    });
+                    root.push(Point {
+                        level: level + 1,
+                        value: Node::None,
+                    });
+                    current += 1;
+                }
+                left_most = end;
+            } else {
+                let end = root.len();
+                for i in 0..*c {
+                    root[left_most].value = Node::Value(symbol[(i + symbol_offset) as usize]);
+                    last_value = left_most;
+                    left_most += 1;
+                    while left_most < end {
+                        if root[left_most].level == level {
+                            break;
+                        }
+                        left_most += 1;
+                    }
+                }
+
+                symbol_offset += *c;
+                if left_most >= end {
+                    unreachable!()
+                }
+                let pos = root.len();
+
+                root[left_most].value = Node::Split(pos as u32, (pos + 1) as u32);
+                root.push(Point {
+                    level: level + 1,
+                    value: Node::None,
+                });
+                root.push(Point {
+                    level: level + 1,
+                    value: Node::None,
+                });
+
+                let mut current = left_most + 1;
+                left_most = pos;
+
+                while current < end {
+                    if root[current].level != level {
+                        current += 1;
+                        continue;
+                    }
+                    let pos = root.len();
+                    root[current].value = Node::Split(pos as u32, (pos + 1) as u32);
+                    root.push(Point {
+                        level: level + 1,
+                        value: Node::None,
+                    });
+                    root.push(Point {
+                        level: level + 1,
+                        value: Node::None,
+                    });
+                    current += 1;
+                }
             }
+        }
+
+        Self {
+            tree: root
+                .into_iter()
+                .take(last_value + 1)
+                .map(|x| x.value)
+                .collect(),
         }
     }
 }
@@ -105,5 +216,52 @@ mod tests {
     fn it_works() {
         let f = include_bytes!("profile.jpg");
         decode(f);
+    }
+
+    #[test]
+    fn test_huffman_0() {
+        let tree = HuffmanTree::new(&[0, 1], &[1]);
+        use Node::*;
+        let expected = vec![Split(1, 2), Split(3, 4), Split(5, 6), Value(1)];
+        assert_eq!(expected, tree.tree);
+    }
+
+    #[test]
+    fn test_huffman_1() {
+        let tree = HuffmanTree::new(&[1], &[1]);
+        use Node::*;
+        let expected = vec![Split(1, 2), Value(1)];
+        assert_eq!(expected, tree.tree);
+    }
+
+    #[test]
+    fn test_huffman_2() {
+        let counts = &[0, 2, 2, 3, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        let elements = &[5, 6, 3, 4, 2, 7, 8, 1, 0, 9];
+        let tree = HuffmanTree::new(counts, elements);
+        use Node::*;
+        let expected: Vec<Node> = vec![
+            Split(1, 2),
+            Split(3, 4),
+            Split(5, 6),
+            Value(5),
+            Value(6),
+            Split(7, 8),
+            Split(9, 10),
+            Value(3),
+            Value(4),
+            Split(11, 12),
+            Split(13, 14),
+            Value(2),
+            Value(7),
+            Value(8),
+            Split(15, 16),
+            Value(1),
+            Split(17, 18),
+            Value(0),
+            Split(19, 20),
+            Value(9),
+        ];
+        assert_eq!(expected, tree.tree);
     }
 }
