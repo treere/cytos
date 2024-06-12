3e342cfda910740f111898ea38e3e01000cf4a92
treere <tomasiandrea.at@gmail.com>
2024-06-12 22:50:18 +0200

    Add receiver

--- proph/src/architecture/runner.rs
+++ proph/src/architecture/runner.rs
@@ -3,7 +3,7 @@ use crate::loader::GraphRepr;
 use super::graph::Graph;
 use super::{Result, Value};
 
-use std::sync::mpsc::{channel, Sender};
+use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread::{self, JoinHandle};
 
 pub enum Command {
@@ -15,16 +15,19 @@ pub enum Command {
     ListOutputs(String),
     Dump(String, String),
     Load(String, String, Value),
+    Listener,
 }
 
 pub enum Response {
     Data(Result<Value>),
+    Receiver(Receiver<Response>),
 }
 
 impl std::fmt::Debug for Response {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Response::Data(data) => write!(f, "{:?}", data),
+            Response::Receiver(_) => write!(f, "Receiver"),
         }
     }
 }
@@ -38,12 +41,28 @@ impl Message {
     fn set_resp(&mut self, resp: Result<impl Into<Value>>) {
         self.resp = Some(Response::Data(resp.map(|v| v.into())));
     }
+
+    fn set_listener(&mut self, recv: Receiver<Response>) {
+        self.resp = Some(Response::Receiver(recv));
+    }
 }
 
 impl Drop for Message {
     fn drop(&mut self) {
         let resp = self.resp.take().unwrap_or(Response::Data(Ok(Value::Null)));
         self.sender.send(resp).expect("cannot send");
+    }
+}
+
+struct Listener {
+    sender: Sender<Response>,
+}
+
+impl Listener {
+    fn send(&self) -> Result<()> {
+        self.sender
+            .send(Response::Data(Ok(Value::Null)))
+            .map_err(|_| "cannot send")
     }
 }
 
@@ -57,36 +76,57 @@ impl Runner {
         let (sender, receiver) = channel::<(Command, Message)>();
         Self {
             thread: thread::spawn(move || {
-                let mut graph = repr.build(&reg).expect("Cannot build graph");
+                let mut listeners: Vec<Listener> = Vec::new();
                 {
-                    loop {
-                        while let Ok((command, mut message)) = receiver.recv() {
-                            match command {
-                                Command::Start => break,
-                                Command::Status => message.set_resp(Ok("Idle")),
-                                _ => {
-                                    Self::dispatch_command(command, &mut message, &mut graph);
-                                }
-                            }
-                        }
-
-                        graph.initialize().expect("cannot initialize");
-                        'outer: loop {
-                            while let Ok((command, mut message)) = receiver.try_recv() {
+                    let mut graph = repr.build(&reg).expect("Cannot build graph");
+                    {
+                        loop {
+                            while let Ok((command, mut message)) = receiver.recv() {
                                 match command {
-                                    Command::Stop => break 'outer,
-                                    Command::Status => message.set_resp(Ok("Running")),
+                                    Command::Start => break,
+                                    Command::Status => message.set_resp(Ok("Idle")),
+                                    Command::Listener => {
+                                        let (s, r) = channel::<Response>();
+                                        message.set_listener(r);
+                                        listeners.push(Listener { sender: s })
+                                    }
                                     _ => {
                                         Self::dispatch_command(command, &mut message, &mut graph);
                                     }
                                 }
                             }
 
-                            graph.step().expect("cannot step")
+                            graph.initialize().expect("cannot initialize");
+                            'outer: loop {
+                                while let Ok((command, mut message)) = receiver.try_recv() {
+                                    match command {
+                                        Command::Stop => break 'outer,
+                                        Command::Status => message.set_resp(Ok("Running")),
+                                        Command::Listener => {
+                                            let (s, r) = channel::<Response>();
+                                            message.set_listener(r);
+                                            listeners.push(Listener { sender: s })
+                                        }
+
+                                        _ => {
+                                            Self::dispatch_command(
+                                                command,
+                                                &mut message,
+                                                &mut graph,
+                                            );
+                                        }
+                                    }
+                                }
+
+                                graph.step().expect("cannot step");
+                                for l in &listeners {
+                                    l.send().expect("cannot sent to listener");
+                                }
+                            }
+                            graph.terminate().expect("cannot terminate");
                         }
-                        graph.terminate().expect("cannot terminate");
                     }
-                }
+                };
             }),
             sender,
         }
@@ -112,6 +152,7 @@ impl Runner {
             Command::Start => (),
             Command::Stop => (),
             Command::Status => (),
+            Command::Listener => (),
             Command::ListNodes => message.set_resp(Ok(graph.list_nodes())),
             Command::ListInputs(node) => message.set_resp(graph.list_node_inputs(&node)),
             Command::ListOutputs(node) => message.set_resp(graph.list_node_outputs(&node)),
