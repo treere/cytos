da9ec41a21aeafa38d27d613e8b3a45dedcc0b32
treere <tomasiandrea.at@gmail.com>
2024-06-18 10:01:48 +0200

    Move value in a mod

--- proph/src/architecture/mod.rs
+++ proph/src/architecture/mod.rs
@@ -4,12 +4,15 @@ pub mod graph;
 pub mod props;
 pub mod runner;
 mod traits;
+mod value;
 
 pub use self::props::{Dumper, InputProp, OutputProp};
 pub use self::traits::{Stepper, Transformer};
+pub use value::Value;
 
 pub type NodeId = u64;
 pub type ParamId = u64;
 
 pub type Result<T> = std::result::Result<T, &'static str>;
-pub type Value = serde_json::Value;
+
+
---
+++ proph/src/architecture/value.rs
@@ -0,0 +1,1 @@
+pub type Value = serde_json::Value;
