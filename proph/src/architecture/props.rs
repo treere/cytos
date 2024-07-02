bd6578e57f4ea26d1357a7c1c8c9df9570946889
treere <tomasiandrea.at@gmail.com>
2024-07-02 11:17:49 +0200

    Simpler prop

--- proph/src/architecture/props.rs
+++ proph/src/architecture/props.rs
@@ -8,73 +8,105 @@ pub trait Dump {
     fn dump(&self) -> Result<Value>;
 }
 
-pub struct Dumper {
-    prop: Box<dyn Dump>,
-}
+pub struct Dumper(Box<dyn Dump>);
 
 impl Dumper {
     pub fn dump(&self) -> Result<Value> {
-        self.prop.dump()
+        self.0.dump()
     }
 }
 
-/// A property
-pub struct InputProp<T> {
-    val: Rc<UnsafeCell<T>>,
-}
+struct Prop<T>(Rc<UnsafeCell<T>>);
 
-impl<T: 'static> InputProp<T> {
+impl<T: 'static> Prop<T> {
     pub fn new(val: T) -> Self {
-        Self {
-            val: Rc::new(UnsafeCell::new(val)),
-        }
+        Self(Rc::new(UnsafeCell::new(val)))
     }
 
     pub fn get(&self) -> &T {
-        unsafe { &*self.val.get() }
+        unsafe { &*self.0.get() }
+    }
+
+    pub fn set(&mut self) -> &mut T {
+        unsafe { &mut *self.0.get() }
     }
 
     pub fn change_value(&mut self, val: GenericOutputProp) -> Result<()> {
-        if let Ok(v) = val.prop.downcast::<UnsafeCell<T>>() {
-            self.val = v;
+        if let Ok(v) = val.0 .0.downcast::<UnsafeCell<T>>() {
+            self.0 = v;
             Ok(())
         } else {
             Err("invalid type")
         }
     }
 
-    pub fn as_generic(&self) -> GenericInputProp {
-        GenericInputProp {
-            prop: self.val.clone(),
-        }
+    pub fn as_generic(&self) -> GenericProp {
+        GenericProp(self.0.clone())
     }
 }
 
-impl<T: 'static + DeserializeOwned> InputProp<T> {
+impl<T: 'static + DeserializeOwned> Prop<T> {
     pub fn load(&mut self, val: Value) -> Result<()> {
         let value = val.convert()?;
-        self.val = Rc::new(UnsafeCell::new(value));
+        self.0 = Rc::new(UnsafeCell::new(value));
         Ok(())
     }
 }
 
-impl<T: 'static + Serialize> Dump for InputProp<T> {
+impl<T: 'static + Serialize> Dump for Prop<T> {
     fn dump(&self) -> Result<Value> {
         Value::from_t(self.get())
     }
 }
 
-impl<T: 'static + Serialize> InputProp<T> {
-    pub fn dump(&self) -> Result<Value> {
-        Value::from_t(self.get())
+impl<T: 'static + Serialize> Prop<T> {
+    pub fn as_dumper(&self) -> Dumper {
+        Dumper(Box::new(Self(self.0.clone())))
+    }
+}
+
+impl<T: Default + 'static> Default for Prop<T> {
+    fn default() -> Self {
+        Self::new(T::default())
+    }
+}
+
+/// A property
+pub struct InputProp<T>(Prop<T>);
+
+impl<T: 'static> InputProp<T> {
+    pub fn new(val: T) -> Self {
+        Self(Prop::new(val))
     }
 
+    pub fn get(&self) -> &T {
+        self.0.get()
+    }
+
+    pub fn change_value(&mut self, val: GenericOutputProp) -> Result<()> {
+        self.0.change_value(val)
+    }
+
+    pub fn as_generic(&self) -> GenericInputProp {
+        GenericInputProp(self.0.as_generic())
+    }
+}
+
+impl<T: 'static + DeserializeOwned> InputProp<T> {
+    pub fn load(&mut self, val: Value) -> Result<()> {
+        self.0.load(val)
+    }
+}
+
+impl<T: 'static + Serialize> Dump for InputProp<T> {
+    fn dump(&self) -> Result<Value> {
+        self.0.dump()
+    }
+}
+
+impl<T: 'static + Serialize> InputProp<T> {
     pub fn as_dumper(&self) -> Dumper {
-        Dumper {
-            prop: Box::new(Self {
-                val: self.val.clone(),
-            }),
-        }
+        self.0.as_dumper()
     }
 }
 
@@ -84,29 +116,23 @@ impl<T: Default + 'static> Default for InputProp<T> {
     }
 }
 
-pub struct OutputProp<T> {
-    val: Rc<UnsafeCell<T>>,
-}
+pub struct OutputProp<T>(Prop<T>);
 
 impl<T: 'static> OutputProp<T> {
     pub fn new(val: T) -> Self {
-        Self {
-            val: Rc::new(UnsafeCell::new(val)),
-        }
+        Self(Prop::new(val))
     }
 
     pub fn get(&self) -> &T {
-        unsafe { &*self.val.get() }
+        self.0.get()
     }
 
     pub fn set(&mut self) -> &mut T {
-        unsafe { &mut *self.val.get() }
+        self.0.set()
     }
 
     pub fn as_generic(&self) -> GenericOutputProp {
-        GenericOutputProp {
-            prop: self.val.clone(),
-        }
+        GenericOutputProp(self.0.as_generic())
     }
 }
 
@@ -117,16 +143,8 @@ impl<T: 'static + Serialize> Dump for OutputProp<T> {
 }
 
 impl<T: 'static + Serialize> OutputProp<T> {
-    pub fn dump(&self) -> Result<Value> {
-        Value::from_t(self.get())
-    }
-
     pub fn as_dumper(&self) -> Dumper {
-        Dumper {
-            prop: Box::new(Self {
-                val: self.val.clone(),
-            }),
-        }
+        self.0.as_dumper()
     }
 }
 
@@ -136,37 +154,40 @@ impl<T: Default + 'static> Default for OutputProp<T> {
     }
 }
 
-/// Generic Property to be casted back
-pub struct GenericOutputProp {
-    prop: Rc<dyn Any>,
+struct GenericProp(Rc<dyn Any>);
+
+impl GenericProp {
+    pub fn try_read<T: 'static>(&self, f: impl Fn(&T)) -> Result<()> {
+        if let Ok(v) = self.0.clone().downcast::<UnsafeCell<T>>() {
+            f(unsafe { &*v.get() });
+            Ok(())
+        } else {
+            Err("wrong type")
+        }
+    }
+
+    pub fn is_same(&self, other: &GenericProp) -> bool {
+        Rc::ptr_eq(&self.0, &other.0)
+    }
 }
+
+/// Generic Property to be casted back
+pub struct GenericOutputProp(GenericProp);
 
 impl GenericOutputProp {
     pub fn try_read<T: 'static>(&self, f: impl Fn(&T)) -> Result<()> {
-        if let Ok(v) = self.prop.clone().downcast::<UnsafeCell<T>>() {
-            f(unsafe { &*v.get() });
-            Ok(())
-        } else {
-            Err("wrong type")
-        }
+        self.0.try_read(f)
     }
 }
 
-pub struct GenericInputProp {
-    prop: Rc<dyn Any>,
-}
+pub struct GenericInputProp(GenericProp);
 
 impl GenericInputProp {
     pub fn try_read<T: 'static>(&self, f: impl Fn(&T)) -> Result<()> {
-        if let Ok(v) = self.prop.clone().downcast::<UnsafeCell<T>>() {
-            f(unsafe { &*v.get() });
-            Ok(())
-        } else {
-            Err("wrong type")
-        }
+        self.0.try_read(f)
     }
 
-    pub fn is_linked_to(&self, other: &GenericOutputProp) -> bool {
-        Rc::ptr_eq(&self.prop, &other.prop)
+    pub fn is_same(&self, other: &GenericOutputProp) -> bool {
+        self.0.is_same(&other.0)
     }
 }
