25872d8ad568d43fbcb67b9c4ccebfdf613c828b
treere <tomasiandrea.at@gmail.com>
2024-07-10 23:53:13 +0200

    Add command to system

--- proph/src/architecture/system.rs
+++ proph/src/architecture/system.rs
@@ -1,19 +1,24 @@
 use crate::loader::{Registry, SystemRepr};
 
-use super::{runner::Runner, GraphId};
+use super::{runner::{Command, Response, Runner}, GraphId, Result};
 
 struct System {
-    _runners: Vec<(GraphId, Runner)>,
+    runners: Vec<(GraphId, Runner)>,
 }
 
 impl System {
     pub fn new(repr: SystemRepr, reg: Registry) -> Self {
         Self {
-            _runners: repr
+            runners: repr
                 .graphs
                 .into_iter()
                 .map(|g| Runner::from_repr(g, reg.clone()))
                 .collect(),
         }
+    }
+
+    pub fn command(&mut self, graph: GraphId, command: Command) -> Result<Response> {
+        let v = self.runners.iter_mut().find(|x| x.0 == graph).ok_or("not found")?;
+        v.1.command(command)
     }
 }
