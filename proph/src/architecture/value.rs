9c716603869a4e6d146a0af5271b710ae777cad3
treere <tomasiandrea.at@gmail.com>
2024-11-05 23:51:41 +0100

    Document value

--- proph/src/architecture/value.rs
+++ proph/src/architecture/value.rs
@@ -2,17 +2,20 @@ use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
 use crate::architecture::Result;
 
+/// Generic value
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(transparent)]
 pub struct Value(serde_json::Value);
 
 impl Value {
+    /// Load into a value
     pub fn load<T: Serialize>(val: &T) -> Result<Self> {
         serde_json::to_value(val)
             .map(Self)
             .or(Err("cannot dump value".into()))
     }
 
+    /// Dump a value
     pub fn dump<T: DeserializeOwned>(self) -> Result<T> {
         serde_json::from_value(self.0).or(Err("cannot load value".into()))
     }
