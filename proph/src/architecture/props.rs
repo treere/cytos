258e38db50fa074301198363225e2f053d13d3de
treere <tomasiandrea.at@gmail.com>
2024-11-05 23:49:19 +0100

    Document props

--- proph/src/architecture/props.rs
+++ proph/src/architecture/props.rs
@@ -4,14 +4,17 @@ use std::{any::Any, cell::UnsafeCell, rc::Rc};
 
 use super::{Result, Value};
 
+/// Internal prop structure
 #[derive(Default)]
 struct Prop<T>(Rc<UnsafeCell<T>>);
 
 impl<T: 'static> Prop<T> {
+    /// Create a prop
     pub fn new(val: T) -> Self {
         Self(Rc::new(UnsafeCell::new(val)))
     }
 
+    /// Link that prop to another prop
     pub fn link_value(&mut self, val: GenericOutputProp) -> Result<()> {
         if let Ok(v) = val.0 .0.downcast::<UnsafeCell<T>>() {
             self.0 = v;
@@ -21,6 +24,7 @@ impl<T: 'static> Prop<T> {
         }
     }
 
+    /// Convert this prop to be a generic prop
     pub fn as_generic(&self) -> GenericProp {
         GenericProp(self.0.clone())
     }
@@ -41,6 +45,7 @@ impl<T> std::ops::DerefMut for Prop<T> {
 }
 
 impl<T: 'static + DeserializeOwned> Prop<T> {
+    /// Load a value into a prop
     pub fn load(&mut self, val: Value) -> Result<()> {
         let value = val.dump()?;
         self.0 = Rc::new(UnsafeCell::new(value));
@@ -49,6 +54,7 @@ impl<T: 'static + DeserializeOwned> Prop<T> {
 }
 
 impl<T: 'static + Serialize> Prop<T> {
+    /// Dump the value of a prop
     fn dump(&self) -> Result<Value> {
         Value::load(&**self)
     }
@@ -61,26 +67,31 @@ impl<T: 'static + Serialize> Prop<T> {
 pub struct InputProp<T>(Prop<T>);
 
 impl<T: 'static> InputProp<T> {
+    /// Create an input prop
     pub fn new(val: T) -> Self {
         Self(Prop::new(val))
     }
 
+    /// Link an input to an output
     pub fn link_value(&mut self, val: GenericOutputProp) -> Result<()> {
         self.0.link_value(val)
     }
 
+    /// Convert this props to a generic prop
     pub fn as_generic(&self) -> GenericInputProp {
         GenericInputProp(self.0.as_generic())
     }
 }
 
 impl<T: 'static + DeserializeOwned> InputProp<T> {
+    /// Load a value
     pub fn load(&mut self, val: Value) -> Result<()> {
         self.0.load(val)
     }
 }
 
 impl<T: 'static + Serialize> InputProp<T> {
+    /// Dump a value
     pub fn dump(&self) -> Result<Value> {
         self.0.dump()
     }
@@ -101,16 +112,19 @@ impl<T: 'static> std::ops::Deref for InputProp<T> {
 pub struct OutputProp<T>(Prop<T>);
 
 impl<T: 'static> OutputProp<T> {
+    /// Create an output prop
     pub fn new(val: T) -> Self {
         Self(Prop::new(val))
     }
 
+    /// Convert an output prop to a generic prop
     pub fn as_generic(&self) -> GenericOutputProp {
         GenericOutputProp(self.0.as_generic())
     }
 }
 
 impl<T: 'static + Serialize> OutputProp<T> {
+    /// Dump the value
     pub fn dump(&self) -> Result<Value> {
         self.0.dump()
     }
@@ -130,18 +144,11 @@ impl<T: 'static> std::ops::DerefMut for OutputProp<T> {
     }
 }
 
+/// Generic prop
 struct GenericProp(Rc<dyn Any>);
 
 impl GenericProp {
-    pub fn try_read<T: 'static>(&self, f: impl Fn(&T)) -> Result<()> {
-        if let Ok(v) = self.0.clone().downcast::<UnsafeCell<T>>() {
-            f(unsafe { &*v.get() });
-            Ok(())
-        } else {
-            Err("wrong type".into())
-        }
-    }
-
+    /// Verify if two generic props are the same
     pub fn is_same(&self, other: &GenericProp) -> bool {
         Rc::ptr_eq(&self.0, &other.0)
     }
@@ -151,18 +158,16 @@ impl GenericProp {
 pub struct GenericOutputProp(GenericProp);
 
 impl GenericOutputProp {
-    pub fn try_read<T: 'static>(&self, f: impl Fn(&T)) -> Result<()> {
-        self.0.try_read(f)
+    /// Verify if two generic props are the same
+    pub fn is_same(&self, other: &GenericInputProp) -> bool {
+        self.0.is_same(&other.0)
     }
 }
 
 pub struct GenericInputProp(GenericProp);
 
 impl GenericInputProp {
-    pub fn try_read<T: 'static>(&self, f: impl Fn(&T)) -> Result<()> {
-        self.0.try_read(f)
-    }
-
+    /// Verify if two generic props are the same
     pub fn is_same(&self, other: &GenericOutputProp) -> bool {
         self.0.is_same(&other.0)
     }
