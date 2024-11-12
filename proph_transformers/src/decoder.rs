e0e65301e6ecd97639c894cf56fb27bf8b600ab7
treere <tomasiandrea.at@gmail.com>
2024-11-12 22:38:02 +0100

    Avoid copy of the frame

--- proph_transformers/src/decoder.rs
+++ proph_transformers/src/decoder.rs
@@ -2,6 +2,8 @@ use proph::architecture::{InputProp, OutputProp, Result, Stepper};
 use proph_derive::TransFn;
 use serde::{Deserialize, Serialize};
 
+use crate::source::Frame;
+
 #[derive(Default, Serialize, Deserialize, Clone)]
 pub struct Image {
     pub width: u32,
@@ -11,14 +13,14 @@ pub struct Image {
 
 #[derive(TransFn, Default)]
 pub struct ImageDecoder {
-    frame: InputProp<Vec<u8>>,
+    frame: InputProp<Frame>,
 
     decoded: OutputProp<Image>,
 }
 
 impl Stepper for ImageDecoder {
     fn step(&mut self) -> Result<()> {
-        if let Ok(image) = image::load_from_memory(&self.frame[..]) {
+        if let Ok(image) = image::load_from_memory(self.frame.as_u8()) {
             let gray = image.to_luma8();
             let width = gray.width();
             let height = gray.height();
--- proph_transformers/src/source.rs
+++ proph_transformers/src/source.rs
@@ -1,6 +1,82 @@
 use proph::architecture::{InputProp, OutputProp, Result, Stepper};
 use proph_derive::TransFn;
 use rscam::Camera;
+use serde::{ser::SerializeSeq, Deserialize, Serialize};
+
+pub enum FrameKind {
+    Rscam(rscam::Frame),
+    Raw(Vec<u8>),
+}
+
+impl serde::Serialize for FrameKind {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        match self {
+            FrameKind::Rscam(frame) => {
+                let mut s = serializer.serialize_seq(Some(frame.len()))?;
+                for i in frame.iter() {
+                    s.serialize_element(i)?;
+                }
+                s.end()
+            }
+            FrameKind::Raw(_vec) => todo!(),
+        }
+    }
+}
+
+struct FrameKindVisitor;
+
+impl<'de> serde::de::Visitor<'de> for FrameKindVisitor {
+    type Value = FrameKind;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("A FrameKind")
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> std::result::Result<Self::Value, A::Error>
+    where
+        A: serde::de::SeqAccess<'de>,
+    {
+        let mut v = vec![];
+        while let Some(s) = seq.next_element()? {
+            v.push(s);
+        }
+        Ok(FrameKind::Raw(v))
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for FrameKind {
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_seq(FrameKindVisitor)
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Frame {
+    frame: FrameKind,
+}
+
+impl Default for Frame {
+    fn default() -> Self {
+        Self {
+            frame: FrameKind::Raw(Vec::default()),
+        }
+    }
+}
+
+impl Frame {
+    pub fn as_u8(&self) -> &[u8] {
+        match self.frame {
+            FrameKind::Rscam(ref frame) => frame,
+            FrameKind::Raw(ref vec) => &vec[..],
+        }
+    }
+}
 
 #[derive(TransFn)]
 pub struct Rscam {
@@ -8,7 +84,7 @@ pub struct Rscam {
     interval: InputProp<(u32, u32)>,
     resolution: InputProp<(u32, u32)>,
 
-    frame: OutputProp<Vec<u8>>,
+    frame: OutputProp<Frame>,
 
     camera: Option<Camera>,
 }
@@ -19,7 +95,7 @@ impl Default for Rscam {
             filename: InputProp::new("/dev/video0".to_owned()),
             interval: InputProp::new((1, 30)),
             resolution: InputProp::new((1280, 720)),
-            frame: OutputProp::new(Vec::default()),
+            frame: OutputProp::new(Frame::default()),
             camera: None,
         }
     }
@@ -30,7 +106,9 @@ impl Stepper for Rscam {
         if let Some(camera) = self.camera.as_ref() {
             let frame = camera.capture().or(Err("cannot capture"))?;
 
-            *self.frame = frame.iter().copied().collect::<Vec<_>>();
+            *self.frame = Frame {
+                frame: FrameKind::Rscam(frame),
+            };
 
             Ok(())
         } else {
