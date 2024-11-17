d8d8fe1bf3fb0d234fffa6f109b8882933f62eb5
treere <tomasiandrea.at@gmail.com>
2024-11-17 22:18:07 +0100

    Implement params page

---
+++ proph_web_fe/src/routes/graphs/[graph]/nodes/[node]/params/[param]/+page.js
@@ -0,0 +1,11 @@
+export async function load({ fetch, params }) {
+	const graph = params.graph;
+	const node = params.node;
+	const param = params.param;
+	return {
+		graph,
+		node,
+		param,
+		value: fetch(`/api/graphs/${graph}/nodes/${node}/params/${param}/dump`).then((x) => x.json())
+	};
+}
--- proph_web_fe/src/routes/graphs/[graph]/nodes/[node]/params/[param]/+page.svelte
+++ proph_web_fe/src/routes/graphs/[graph]/nodes/[node]/params/[param]/+page.svelte
@@ -1,1 +1,25 @@
-ciao!
+<script>
+	let { data } = $props();
+</script>
+
+<div>
+	Graph: {data.graph}
+</div>
+
+<div>
+	Node: {data.node}
+</div>
+
+<div>
+	Param: {data.param}
+</div>
+
+<div>
+	Value: {#await data.value}
+		loading
+	{:then value}
+		{JSON.stringify(value, null, 2)}
+	{:catch}
+		Error
+	{/await}
+</div>
