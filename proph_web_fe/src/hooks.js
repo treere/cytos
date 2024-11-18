7faf783545b6e0813892cea9d9ad8a73d758748f
treere <tomasiandrea.at@gmail.com>
2024-11-18 22:43:34 +0100

    Fix hooks

---
+++ proph_web_fe/src/hooks.js
@@ -0,0 +1,12 @@
+export async function handle({ event, resolve }) {
+	if (event.url.pathname.startsWith('/api')) {
+		return fetch(`http://localhost:3000${event.url.pathname.replace(/^\/api/, '')}`, {
+			body: event.request.body,
+			method: event.request.method,
+			headers: event.request.headers
+		});
+	}
+
+	const response = await resolve(event);
+	return response;
+}
--- proph_web_fe/vite.config.ts
+++ proph_web_fe/vite.config.ts
@@ -2,14 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://localhost:3000',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, '')
-			}
-		}
-	}
+	plugins: [sveltekit()]
 });
