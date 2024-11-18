32f4b711c4425623c886a0a4d9d2408fe958616d
treere <tomasiandrea.at@gmail.com>
2024-11-18 23:49:56 +0100

    Remove navbar

--- proph_web_fe/src/routes/+layout.svelte
+++ proph_web_fe/src/routes/+layout.svelte
@@ -4,14 +4,6 @@
 </script>
 
 <div class="mx-auto h-full w-full max-w-screen-lg bg-white p-6 shadow-lg">
-	<nav class="flex h-12 items-center justify-between">
-		<a href="/" class="text-blue-500 hover:text-blue-700"> Home </a>
-		<button
-			onclick={() => window.history.back()}
-			class="rounded-md bg-gray-300 px-4 py-2 text-sm font-medium hover:bg-gray-400">Back</button
-		>
-	</nav>
-
 	<div class="mt-8 flex-grow justify-center">
 		{@render children()}
 	</div>
