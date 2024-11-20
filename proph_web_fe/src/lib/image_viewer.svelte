f3f1894c095679c5ee1da813d2aa1217f3bec1fe
treere <tomasiandrea.at@gmail.com>
2024-11-20 23:10:29 +0100

    Fix types

--- proph_web_fe/src/lib/image_viewer.svelte
+++ proph_web_fe/src/lib/image_viewer.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { rawToImageData, setChild } from '$lib';
-	let { value }: { value: any } = $props();
+	let { value }: { value: Record<string, unknown> } = $props();
 
 	let selectedFormat = $state('jpeg');
 	let formats = ['jpeg', 'raw grey'];
@@ -10,9 +10,9 @@
 	let height = $state('height');
 
 	let renderImage = () => {
-		const binString = Array.from(value[name], (byte: number) => String.fromCodePoint(byte)).join(
-			''
-		);
+		const binString = Array.from(value[name] as number[], (byte: number) =>
+			String.fromCodePoint(byte)
+		).join('');
 		const b64encoded = btoa(binString);
 		const img = document.createElement('img');
 		img.src = 'data:image/jpg;base64,' + b64encoded;
@@ -22,11 +22,14 @@
 	};
 
 	let renderGreyArray = () => {
-		const imageData = rawToImageData(value[width], value[height], value[name]);
-		console.info('B');
+		const w = value[width] as number;
+		const h = value[height] as number;
+		const data = value[name] as number[];
+		const imageData = rawToImageData(w, h, data);
+
 		const canvas = document.createElement('canvas');
-		canvas.width = value[width];
-		canvas.height = value[height];
+		canvas.width = w;
+		canvas.height = h;
 		const ctx = canvas.getContext('2d');
 		canvas.style.width = '800px';
 
