716d665da2d82877c21059a99d3319949dd28f86
treere <tomasiandrea.at@gmail.com>
2024-12-03 23:19:59 +0100

    Removed useless stuff

--- cytos_web_fe/src/routes/map/+page.svelte
+++ cytos_web_fe/src/routes/map/+page.svelte
@@ -1,22 +1,6 @@
 <script lang="ts">
 	import { VisSingleContainer, VisGraph } from '@unovis/svelte';
-
-	type GraphData = {
-		nodes: NodeDatum[];
-		links: LinkDatum[];
-	};
-
-	type NodeDatum = {
-		id?: string;
-		x?: number;
-		y?: number;
-	};
-
-	type LinkDatum = {
-		id?: string;
-		source: number | string | NodeDatum;
-		target: number | string | NodeDatum;
-	};
+	import type { GraphInputNode } from '@unovis/ts';
 
 	const panels = [
 		{ nodes: ['1', '2'], label: 'A' },
@@ -27,9 +11,9 @@
 		{ source: '1', target: '2' },
 		{ source: '2', target: '3' }
 	];
-	const nodeLabel = (n: NodeDatum) => n.id;
+	const nodeLabel = (n: GraphInputNode) => n.id;
 
-	const data: GraphData = { nodes, links };
+	const data = { nodes, links };
 </script>
 
 <VisSingleContainer {data}>
