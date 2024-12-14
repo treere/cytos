527fe19ce98ba18352811aa0e3aed2ac842b9afe
treere <tomasiandrea.at@gmail.com>
2024-12-14 16:35:17 +0100

    Refactor

--- cytos_web_fe/src/routes/map/+page.ts
+++ cytos_web_fe/src/routes/map/+page.ts
@@ -1,30 +1,21 @@
 import type { PageLoad } from './$types';
 
-const zip = (...arrays: any[]) => {
-	const maxLength = Math.max(...arrays.map((x) => x.length));
-	return Array.from({ length: maxLength }).map((_, i) => {
-		return Array.from({ length: arrays.length }, (_, k) => arrays[k][i]);
-	});
-};
-
 export const load: PageLoad = async ({ fetch }) => {
 	const compose = async () => {
 		const graphNames: string[] = await fetch('/api/graphs').then((x) => x.json());
 
-		let nodes: string[][] = await Promise.all(
-			graphNames.map((graph) => fetch(`/api/graphs/${graph}/nodes`).then((x) => x.json()))
+		let nodes = await Promise.all(
+			graphNames.map(async (graph) => {
+				let x = await fetch(`/api/graphs/${graph}/nodes`);
+				return [graph, await x.json()] as const;
+			})
 		);
 
-		let allInternalLinks: string[][] = await Promise.all(
-			graphNames.map((graph) => fetch(`/api/graphs/${graph}/links`).then((x) => x.json()))
-		);
-
-		const panels = zip(graphNames, nodes).map(([g, n]) => ({ nodes: n, label: g }));
+		const panels = nodes.map(([g, n]) => ({ nodes: n, label: g }));
 
 		return {
 			panels,
-			data: { nodes: nodes.flatMap((n) => n).map((n) => ({ id: n })), links: [] },
-			allInternalLinks
+			data: { nodes: nodes.flatMap(([_, n]) => n).map((n) => ({ id: n })), links: [] }
 		};
 	};
 
