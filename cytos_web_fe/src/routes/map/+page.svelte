fc5b42713726f22b89d1e16de7856beed6605639
treere <tomasiandrea.at@gmail.com>
2024-12-15 14:17:26 +0100

    Use different approach

--- cytos_web_fe/src/routes/map/+page.svelte
+++ cytos_web_fe/src/routes/map/+page.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
 	import { VisSingleContainer, VisGraph } from '@unovis/svelte';
-	import type { GraphInputNode } from '@unovis/ts';
-
-	const nodeLabel = (n: GraphInputNode) => n.id;
 
 	interface Props {
 		data: any;
-		panels: any;
+		nodeLabel: any;
+		nodeFill: any;
 	}
 	const { data }: Props = $props();
 </script>
 
 <VisSingleContainer data={data.data}>
-	<VisGraph layoutType="horizontal" panels={data.panels} {nodeLabel} />
+	<VisGraph nodeLabel={data.nodeLabel} nodeFill={data.nodeFill} />
 </VisSingleContainer>
--- cytos_web_fe/src/routes/map/+page.ts
+++ cytos_web_fe/src/routes/map/+page.ts
@@ -1,25 +1,72 @@
+import type { GraphInputNode } from '@unovis/ts';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
 	const compose = async () => {
-		const graphNames: string[] = await fetch('/api/graphs').then((x) => x.json());
+		const data = await fetch('/api/graphs');
+		const graphs: string[] = await data.json();
 
-		let nodes = await Promise.all(
-			graphNames.map(async (graph) => {
-				let x = await fetch(`/api/graphs/${graph}/nodes`);
-				return [graph, await x.json()] as const;
-			})
-		);
+		const processors = (
+			await Promise.all(
+				graphs.map(async (graph) => {
+					const data = await fetch(`/api/graphs/${graph}/nodes`);
+					const json = (await data.json()) as string[];
+					return json.map((node) => `${graph}/${node}`);
+				})
+			)
+		).flatMap((x) => x);
 
-		const panels = nodes.map(([g, n]) => ({ nodes: n, label: g }));
+		const inputs = (
+			await Promise.all(
+				processors.map(async (n) => {
+					const [graph, node] = n.split('/');
+					const data = await fetch(`/api/graphs/${graph}/nodes/${node}/inputs`);
+					const json = (await data.json()) as string[];
+					return json.map((param) => `${graph}/${node}/${param}`);
+				})
+			)
+		).flatMap((x) => x);
+
+		const outputs = (
+			await Promise.all(
+				processors.map(async (n) => {
+					const [graph, node] = n.split('/');
+					const data = await fetch(`/api/graphs/${graph}/nodes/${node}/outputs`);
+					const json = (await data.json()) as string[];
+					return json.map((param) => `${graph}/${node}/${param}`);
+				})
+			)
+		).flatMap((x) => x);
+
+		const nodes = inputs.concat(outputs, processors, graphs);
+
+		const n = nodes.map((x) => ({ id: x }));
+
+		const l = nodes
+			.filter((x) => x.includes('/'))
+			.map((x) => {
+				const lastIndex = x.lastIndexOf('/');
+				const withoutLastPart = lastIndex !== -1 ? x.substring(0, lastIndex) : x;
+				return { source: withoutLastPart, target: x };
+			});
 
 		return {
-			panels,
-			data: { nodes: nodes.flatMap(([_, n]) => n).map((n) => ({ id: n })), links: [] }
+			data: { nodes: n, links: l },
+			nodeLabel: (n: GraphInputNode) => n.id,
+			nodeFill: (n: GraphInputNode) => {
+				switch ((n.id as string).split('/').length) {
+					case 1:
+						return '#ff0000';
+					case 2:
+						return '#00ff00';
+					case 3:
+						return '#0000ff';
+				}
+			}
 		};
 	};
 
 	const res = await compose();
-	console.info(res);
+
 	return res;
 };
